e02e89d20f06859526ab5951876b76b37ee333d3
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2025-12-27 08:11:51 +0530

    Update Assembly_code.c

--- Expression/Assembly_code.c
+++ Expression/Assembly_code.c
@@ -2,15 +2,23 @@
 int main()
 {
     int x = 40, y = 30, a = 45, b = 55;
-    int u;
-   // assembly code for this expression u = x * y + a - b;
+    int u,v;
+   //u = x * y + a - b;
+   //v = (x^y) | (a&b);
    __asm{
     MOV EAX,x
     MUL y
     ADD EAX, a
     SUB EAX, b
     MOV u,EAX
+
+    MOV EAX, x
+    XOR EAX,y
+    MOV EBX,a
+    AND EBX,b
+    OR EAX,EBX
+    MOV v,EAX
    }
-    printf("u = %d", u);
+    printf("u = %d, v = %d", u,v);
  return 0;
 }
