e18f456c3a91a2f208e633ca27c4be87a1269b55
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2025-12-27 08:11:11 +0530

    Update C_code.c

--- Expression/C_code.c
+++ Expression/C_code.c
@@ -2,8 +2,10 @@
 int main()
 {
     int x = 40, y = 30, a = 45, b = 55;
-    int u;
+    int u,v;
    u = x * y + a - b;
-    printf("u = %d", u);
+   v = (x^y) | (a&b);
+
+    printf("u = %d, v = %d", u,v);
  return 0;
 }
